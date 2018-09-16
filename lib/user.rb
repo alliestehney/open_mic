40d29eb224f46db5e2e1ec1c2486c8b07cdde305
alliestehney <allie@quikly.com>
2018-09-16 18:02:07 -0400

    Finished open_mic exercise

--- lib/user.rb
+++ lib/user.rb
@@ -3,5 +3,20 @@ class User
 
   def initialize(name)
     @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(friend, joke)
+    friend.jokes << joke
+  end
+
+  def perform_routine_for(friend)
+    jokes.each do |joke|
+      tell(friend, joke)
+    end  
   end
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,5 +4,8 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
-
+    def test_it_exists
+        joke = Joke.new({ id: 1, question: "Why did the chicken cross the road?", answer: "To get to the other side!" })
+        assert_instance_of Joke, joke
+    end   
 end
